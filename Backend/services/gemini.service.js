adf50b012a5192fd5b35d8b52be04e3822122c50
Mayank-Ratmele <ratmelem@gmail.com>
2025-10-06 00:51:25 +0530

    updated...

--- Backend/services/gemini.service.js
+++ Backend/services/gemini.service.js
@@ -57,14 +57,82 @@ const ai = new GoogleGenAI({
 export const generateResult = async (prompt) => {
 	try {
 		const result = await ai.models.generateContent({
-		model: "gemini-1.5-flash",
+		model: "gemini-2.5-flash",
+		generationConfig: {
+			responseMimeType: "application/json",
+		},
 		systemInstruction: `You are an expert in MERN and Development. 
 		You have 10 years of experience. 
 		You always write modular code, break it into reusable parts, 
 		follow best practices, use understandable comments, 
 		create files as needed, maintain existing functionality, 
 		and handle errors & edge cases gracefully. 
-		Your code is scalable and maintainable.`,
+		Your code is scalable and maintainable.
+		
+		Examples: 
+
+		<example>
+		user: "create an express server"
+		response: {
+		"text": "this is your fileTree structure of the express server",
+		"fileTree": {
+			"app.js": {
+			content: "
+			const express = require('express');
+
+			const app = express();
+
+			app.get('/', (req, res) => {
+				res.send('Hello World');
+			});
+
+			app.listen(3000, () => {
+				console.log('Server is running on port 3000');
+			});
+			"
+			},
+
+			"package.json": {
+			content: "
+			{
+				"name": "temp-server",
+				"version": "1.0.0",
+				"main": "app.js",
+				"scripts": {
+					"test": "echo \"Error: no test specified\" && exit 1"
+				},
+				"keywords": [],
+				"author": "",
+				"license": "ISC",
+				"description": "",
+				"dependencies": {
+					"express": "^5.1.0"
+				}
+			}
+			"
+			}
+			},
+
+			"buildCommand": {
+				mainItem: "npm",
+				commands: ["install"]
+			},
+
+			"startCommand": {
+			mainItem: "node",
+			commands: ["app.js"]
+			}
+		}
+		</example>
+
+		<example>
+			user: "Hello"
+			response: {
+				"text": "Hello, How can I help you today?"
+			}
+		</example>
+
+		`,
 		contents: `${prompt}`,
 		// contents: [{ role: "sender", parts: [{ text: prompt }] }],
 		});
